8f4676e32b6656db7e8e0dd43143a710650b6e8a
NicoHd105 <nico.sausenger@hotmail.com>
2020-11-07 19:11:25 +0100

    Formatting

--- README.md
+++ README.md
@@ -1,19 +1,23 @@
 # Autocopy
+
 Autocopy is a program that can automatically copy a custom file and/or a custom folder, and it's subfolders and subfiles, in an custom interval into a specific directory, into numbered folders with a custom name.
 
 ## Supported Operating System
+
 - Windows 10
 
 ## Setup
+
 - Download the ZIP of this project and extract it.
 - Fill out the the config.json file.
- 
+
 You do not necessarily have to provide a file and a folder, it is ok of you provide just one of them!
 
 ## Example config.json
+
 ```
 {
-    "sourcefilepath": "C:/Folder1/test.txt",  
+    "sourcefilepath": "C:/Folder1/test.txt",
     "sourcefolderpath": "C:/Folder2/test",
     "datafoldername": "Data",
     "datafolderspliter": "_",
@@ -23,5 +27,5 @@ You do not necessarily have to provide a file and a folder, it is ok of you prov
 ```
 
 ## Start
-```node .```
 
+`node .`
--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const chalk = require('chalk');
 const ms = require('ms');
 const { SOURCE_FILE_PATH, SOURCE_FOLDER_PATH, DATA_FOLDER_NAME, DATA_FOLDER_SEPERATOR, DESTINATION_PATH, INTERVALTIME } = require('./config.json');
 
-
 // Making sure the necessary properties have been provided and they are valid
 if (!SOURCE_FILE_PATH && !SOURCE_FOLDER_PATH) return console.log(chalk.red.bold('Neither a source file or a source folder have been provided!'));
 if (SOURCE_FILE_PATH && !fs.existsSync(SOURCE_FILE_PATH)) return console.log(chalk.red.bold(`The source file path ${chalk.yellow(SOURCE_FILE_PATH)} is not a valid path!`));
@@ -12,39 +11,42 @@ if (SOURCE_FOLDER_PATH && !fs.existsSync(SOURCE_FOLDER_PATH)) return console.log
 if (!DATA_FOLDER_NAME) return console.log(chalk.red.bold('No name for the data folders has been provided!'));
 if (!DATA_FOLDER_SEPERATOR) return console.log(chalk.red.bold('No seperator for the data folders has been provided!'));
 if (!DESTINATION_PATH) {
-  return console.log(chalk.red.bold('No destination path has been provided!'));
+	return console.log(chalk.red.bold('No destination path has been provided!'));
 } else if (!fs.existsSync(DESTINATION_PATH.replace(/[^/]*$/, ''))) {
-  return console.log(chalk.red.bold(`The destiantion path ${chalk.yellow(DESTINATION_PATH.replace(/[^/]*$/, ''))} is not a valid path!`));
+	return console.log(chalk.red.bold(`The destiantion path ${chalk.yellow(DESTINATION_PATH.replace(/[^/]*$/, ''))} is not a valid path!`));
 }
 if (!INTERVALTIME) {
-  return console.log(chalk.red.bold('No interval time has been provided!'));
+	return console.log(chalk.red.bold('No interval time has been provided!'));
 } else if (!ms(INTERVALTIME) || /^\d+$/.test(INTERVALTIME)) {
-  return console.log(chalk.red.bold(`The interval time is not a valid number!\nYou can use ${chalk.yellow('d')} for days, ${chalk.yellow('h')} for hours, ${chalk.yellow('m')} for minutes, ${chalk.yellow('s')} for seconds and ${chalk.yellow('ms')} for milliseconds!`));
+	return console.log(
+		chalk.red.bold(
+			`The interval time is not a valid number!\nYou can use ${chalk.yellow('d')} for days, ${chalk.yellow('h')} for hours, ${chalk.yellow('m')} for minutes, ${chalk.yellow(
+				's'
+			)} for seconds and ${chalk.yellow('ms')} for milliseconds!`
+		)
+	);
 }
-
 
 // Make a directory for the data if it does not exist yet
 manager.mkdir(DESTINATION_PATH);
 
-
 // Do the process once at startup
 manager.COPY();
 
-
 // Set an interval for copying the file and/or the folder after a specific amount of time
 setInterval(() => {
-  // Making sure the necessary paths are are still valid
-  if (SOURCE_FILE_PATH && !fs.existsSync(SOURCE_FILE_PATH)) throw console.log(chalk.red.bold(`The source file path ${chalk.yellow(SOURCE_FILE_PATH)} is not a valid path anymore!`));
-  if (SOURCE_FOLDER_PATH && !fs.existsSync(SOURCE_FOLDER_PATH)) throw console.log(chalk.red.bold(`The source folder path ${chalk.yellow(SOURCE_FOLDER_PATH)} is not a valid path anymore!`));
-  if (!fs.existsSync(DESTINATION_PATH.replace(/[^/]*$/, ''))) throw console.log(chalk.red.bold(`The destiantion path ${chalk.yellow(DESTINATION_PATH.replace(/[^/]*$/, ''))} is not a valid path anymore!`));
+	// Making sure the necessary paths are are still valid
+	if (SOURCE_FILE_PATH && !fs.existsSync(SOURCE_FILE_PATH)) throw console.log(chalk.red.bold(`The source file path ${chalk.yellow(SOURCE_FILE_PATH)} is not a valid path anymore!`));
+	if (SOURCE_FOLDER_PATH && !fs.existsSync(SOURCE_FOLDER_PATH)) throw console.log(chalk.red.bold(`The source folder path ${chalk.yellow(SOURCE_FOLDER_PATH)} is not a valid path anymore!`));
+	if (!fs.existsSync(DESTINATION_PATH.replace(/[^/]*$/, '')))
+		throw console.log(chalk.red.bold(`The destiantion path ${chalk.yellow(DESTINATION_PATH.replace(/[^/]*$/, ''))} is not a valid path anymore!`));
 
-  // Copy
-  manager.COPY();
+	// Copy
+	manager.COPY();
 }, ms(INTERVALTIME)); // Interval time
 
-
-// Handling errors 
+// Handling errors
 process.on('unhandledRejection', error => {
-  if (!error) return process.exit();
-  return console.error('Unhandled promise rejection:', error);
+	if (!error) return process.exit();
+	return console.error('Unhandled promise rejection:', error);
 });
